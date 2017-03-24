2aacdfa798930020228555d9af88dbe127b94d35
Michał <ms09081995@o2.pl>
2017-03-24 14:45:25 +0100

    test

--- PZ_Projekt/Program.cs
+++ PZ_Projekt/Program.cs
@@ -17,6 +17,7 @@ namespace PZ_Projekt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
