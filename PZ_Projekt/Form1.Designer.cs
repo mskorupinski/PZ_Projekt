b00c174215b8e8cca123bfdb069be860d6997015
Michał <ms09081995@o2.pl>
2017-03-28 17:47:49 +0200

    Dodawanie plików

--- PZ_Projekt/Form1.Designer.cs
+++ PZ_Projekt/Form1.Designer.cs
@@ -28,76 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialogFile1 = new System.Windows.Forms.OpenFileDialog();
+            this.labelFiles = new System.Windows.Forms.Label();
+            this.labelFile1 = new System.Windows.Forms.Label();
+            this.labelFile2 = new System.Windows.Forms.Label();
+            this.buttonAddFile = new System.Windows.Forms.Button();
+            this.buttonDelete1 = new System.Windows.Forms.Button();
+            this.buttonDelete2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
+            // openFileDialogFile1
+            // 
+            this.openFileDialogFile1.FileName = "openFileDialogFile1";
+            // 
+            // labelFiles
+            // 
+            this.labelFiles.AutoSize = true;
+            this.labelFiles.Location = new System.Drawing.Point(22, 8);
+            this.labelFiles.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFiles.Name = "labelFiles";
+            this.labelFiles.Size = new System.Drawing.Size(66, 13);
+            this.labelFiles.TabIndex = 0;
+            this.labelFiles.Text = "Dodane pliki";
+            // 
+            // labelFile1
+            // 
+            this.labelFile1.AutoSize = true;
+            this.labelFile1.Location = new System.Drawing.Point(22, 21);
+            this.labelFile1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFile1.Name = "labelFile1";
+            this.labelFile1.Size = new System.Drawing.Size(39, 13);
+            this.labelFile1.TabIndex = 1;
+            this.labelFile1.Text = "Plik1:  ";
+            this.labelFile1.Visible = false;
+            // 
+            // labelFile2
+            // 
+            this.labelFile2.AutoSize = true;
+            this.labelFile2.Location = new System.Drawing.Point(22, 34);
+            this.labelFile2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFile2.Name = "labelFile2";
+            this.labelFile2.Size = new System.Drawing.Size(39, 13);
+            this.labelFile2.TabIndex = 2;
+            this.labelFile2.Text = "Plik2:  ";
+            this.labelFile2.Visible = false;
+            // 
+            // buttonAddFile
+            // 
+            this.buttonAddFile.Location = new System.Drawing.Point(25, 49);
+            this.buttonAddFile.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAddFile.Name = "buttonAddFile";
+            this.buttonAddFile.Size = new System.Drawing.Size(63, 21);
+            this.buttonAddFile.TabIndex = 3;
+            this.buttonAddFile.Text = "Dodaj plik";
+            this.buttonAddFile.UseVisualStyleBackColor = true;
+            this.buttonAddFile.Click += new System.EventHandler(this.buttonAddFile_Click);
+            // 
+            // buttonDelete1
+            // 
+            this.buttonDelete1.Location = new System.Drawing.Point(469, 16);
+            this.buttonDelete1.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDelete1.Name = "buttonDelete1";
+            this.buttonDelete1.Size = new System.Drawing.Size(42, 22);
+            this.buttonDelete1.TabIndex = 4;
+            this.buttonDelete1.Text = "Usuń";
+            this.buttonDelete1.UseVisualStyleBackColor = true;
+            this.buttonDelete1.Visible = false;
+            this.buttonDelete1.Click += new System.EventHandler(this.buttonDelete1_Click);
+            // 
+            // buttonDelete2
+            // 
+            this.buttonDelete2.Location = new System.Drawing.Point(469, 34);
+            this.buttonDelete2.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDelete2.Name = "buttonDelete2";
+            this.buttonDelete2.Size = new System.Drawing.Size(42, 22);
+            this.buttonDelete2.TabIndex = 5;
+            this.buttonDelete2.Text = "Usuń";
+            this.buttonDelete2.UseVisualStyleBackColor = true;
+            this.buttonDelete2.Visible = false;
+            this.buttonDelete2.Click += new System.EventHandler(this.buttonDelete2_Click);
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 87);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(25, 75);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(267, 37);
+            this.button1.Size = new System.Drawing.Size(200, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Otwórz plik 1";
+            this.button1.Text = "Generuj";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Enabled = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(625, 87);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(267, 37);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Otwórz plik 2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 174);
+            this.dataGridView1.Location = new System.Drawing.Point(25, 141);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(562, 500);
+            this.dataGridView1.Size = new System.Drawing.Size(422, 406);
             this.dataGridView1.TabIndex = 4;
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(625, 174);
+            this.dataGridView2.Location = new System.Drawing.Point(469, 141);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(614, 500);
+            this.dataGridView2.Size = new System.Drawing.Size(460, 406);
             this.dataGridView2.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1268, 738);
+            this.ClientSize = new System.Drawing.Size(951, 600);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Controls.Add(this.buttonDelete2);
+            this.Controls.Add(this.buttonDelete1);
+            this.Controls.Add(this.buttonAddFile);
+            this.Controls.Add(this.labelFiles);
+            this.Controls.Add(this.labelFile1);
+            this.Controls.Add(this.labelFile2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.OpenFileDialog openFileDialogFile1;
+        private System.Windows.Forms.Label labelFiles;
+        private System.Windows.Forms.Label labelFile1;
+        private System.Windows.Forms.Label labelFile2;
+        private System.Windows.Forms.Button buttonAddFile;
+        private System.Windows.Forms.Button buttonDelete1;
+        private System.Windows.Forms.Button buttonDelete2;
+
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
     }
--- PZ_Projekt/Form1.cs
+++ PZ_Projekt/Form1.cs
@@ -13,6 +13,13 @@ namespace PZ_Projekt
 {
     public partial class Form1 : Form
     {
+        public string _File1Stream { get; set; }
+        public string _File2Stream { get; set; }
+
+        public string _File1Name { get; set; }
+        public string _File2Name { get; set; }
+
+
         DataTable table = new DataTable();
         DataTable table2 = new DataTable();
         public Form1()
@@ -31,41 +38,145 @@ namespace PZ_Projekt
 
         List<string> one;
         List<string> two;
+
+        private void buttonAddFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogFile1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialogFile1.FileName.IndexOf(".out") > -1)
+                {
+                    if (string.IsNullOrEmpty(_File1Stream))
+                    {
+                        _File1Stream = openFileDialogFile1.FileName;
+                        _File1Name = _File1Stream.Substring(_File1Stream.LastIndexOf('\\') + 1);
+                        labelFile1.Text += _File1Name;
+                        labelFile1.Visible = true;
+                        buttonDelete1.Visible = true;
+
+                    }
+                    else if (string.IsNullOrEmpty(_File1Stream) || string.IsNullOrEmpty(_File2Stream))
+                    {
+                        if (_File1Stream != openFileDialogFile1.FileName)
+                        {
+                            _File2Stream = openFileDialogFile1.FileName;
+                            _File2Name = _File2Stream.Substring(_File2Stream.LastIndexOf('\\') + 1);
+                            labelFile2.Text += _File2Name;
+                            labelFile2.Visible = true;
+                            buttonDelete2.Visible = true;
+                            button1.Enabled = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Podany plik już instnieje", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dodano maksymalna liczbe plików", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Błędny format pliku", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+        }
+
+        private void buttonDelete1_Click(object sender, EventArgs e)
+        {
+            labelFile1.Text = "Plik1:  ";
+            if (string.IsNullOrEmpty(this._File2Stream))
+            {
+                this._File1Name = null;
+                this._File1Stream = null;
+                labelFile1.Visible = false;
+                buttonDelete1.Visible = false;
+            }
+            else
+            {
+                labelFile2.Text = "Plik2:  ";
+                this._File1Name = this._File2Name;
+                labelFile1.Text += this._File1Name;
+                this._File1Stream = this._File2Stream;
+                this._File2Name = null;
+                this._File2Stream = null;
+                labelFile2.Visible = false;
+                buttonDelete2.Visible = false;
+            }
+        }
+
+        private void buttonDelete2_Click(object sender, EventArgs e)
+        {
+            labelFile2.Text = "Plik2:";
+            this._File2Name = null;
+            this._File2Stream = null;
+            labelFile2.Visible = false;
+            buttonDelete2.Visible = false;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            System.IO.StreamReader sr = new
+               System.IO.StreamReader(this._File1Stream);
+            List<string> temp = new List<string>();
+            while (!sr.EndOfStream)
             {
-                System.IO.StreamReader sr = new
-                   System.IO.StreamReader(openFileDialog1.FileName);
-                List<string> temp= new List<string>();
-                while (!sr.EndOfStream)
-                {
-                   temp.Add(sr.ReadLine());
-                }
-                one = dane(temp.ToArray<string>());
-                int licznik =0;
-                for (int i=0; i<one.Count; i+=4)
-                {
-                    /*
-                    if (licznik == 4)
-                    {
-
-                        richTextBox1.Text += "\n";
-                        licznik = 0;
-                    }
-                    */
-                    //richTextBox1.Text+= temp2.ToString();
-                    //richTextBox1.Text += " ";
-                    table.Rows.Add(one[i], one[i+2],one[i+3]);
-                    licznik++;
-
-                }
-                dataGridView1.DataSource = table;
-                sr.Close();
+                temp.Add(sr.ReadLine());
             }
+            one = dane(temp.ToArray<string>());
+            int licznik = 0;
+            for (int i = 0; i < one.Count; i += 4)
+            {
+                /*
+                if (licznik == 4)
+                {
 
+                    richTextBox1.Text += "\n";
+                    licznik = 0;
+                }
+                */
+                //richTextBox1.Text+= temp2.ToString();
+                //richTextBox1.Text += " ";
+                table.Rows.Add(one[i], one[i + 2], one[i + 3]);
+                licznik++;
+
+            }
+            dataGridView1.DataSource = table;
+            sr.Close();
+
+            sr = new
+               System.IO.StreamReader(_File2Stream);
+            temp = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                temp.Add(sr.ReadLine());
+            }
+            two = dane(temp.ToArray<string>());
+
+            for (int i = 0; i < two.Count; i += 4)
+            {
+                /*
+                if (licznik == 4)
+                {
+
+                    richTextBox1.Text += "\n";
+                    licznik = 0;
+                }
+                */
+                //richTextBox1.Text+= temp2.ToString();
+                //richTextBox1.Text += " ";
+                table2.Rows.Add(two[i], two[i + 2], two[i + 3]);
+
+
+            }
+            dataGridView2.DataSource = table2;
+            sr.Close();
         }
+
+        
+
+
 
         private void button2_Click(object sender, EventArgs e)
         {
